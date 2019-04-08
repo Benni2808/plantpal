af8eca74f0d51a48b44935c7b1b05354829aaf41
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-04-08 15:19:40 +0200

    create relationship user-plants

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,4 +1,5 @@
 class Plant < ApplicationRecord
+    belongs_to :user
     has_one_attached :image
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create([
+#     {
+#         email: 'test@example.at',
+#         password: 'slfjsflj2441!j',
+#         nickName: 'suppiduppipflanzenboy'
+#     },
+#     {
+#         email: 'test1@example.at',
+#         password: 'slfjsflj2441!j',
+#         nickName: 'suppiduppipflanzenboyy'
+#     }
+# ])
+
+Plant.create([
+    {
+        realName: 'sljfslfj',
+        nickName: 'plant1',
+        waterNeed: 1400,
+        waterCurrent: 600,
+        sunNeed: 2.5,
+        place: 'bedroom',
+        love: 6
+    },
+    {
+        realName: 'sfsf',
+        nickName: 'plant2',
+        waterNeed: 1200,
+        waterCurrent: 600,
+        sunNeed: 2.5,
+        place: 'living room',
+        love: 3
+    }
+])
