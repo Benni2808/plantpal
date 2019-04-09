fd101f7c972888c0a6823adf5c01d6dc0f1b304f
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-04-09 13:43:48 +0200

    redirect

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,6 @@
 class PagesController < ApplicationController
   def home
-    if !current_user
-      redirect_to new_user_session_path, notice: 'Not logged in'
-    else
+    if current_user
       redirect_to plants_path, notice: 'Welcome to your dashboard'
     end
   end
