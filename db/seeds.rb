3e8127cf911e16cad2655fb59dc61222f330bbc9
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-04-10 23:38:39 +0200

    update seed file for dokku

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,46 +8,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create([
-#     {
-#         email: 'test@example.at',
-#         password: 'slfjsflj2441!j',
-#         nickName: 'suppiduppipflanzenboy'
-#     },
-#     {
-#         email: 'test1@example.at',
-#         password: 'slfjsflj2441!j',
-#         nickName: 'suppiduppipflanzenboyy'
-#     },
-#     {
-#         email: 'daniela.kubesch@gmail.com',
-#         password: 'test1234',
-#         nickName: 'Daniela'
-#     }
-# ])
-
-Plant.create([
+User.create([
     {
-        realName: 'Rose',
-        nickName: 'Lisa',
-        waterNeed: 1400,
-        waterCurrent: 600,
-        sunNeed: 2.5,
-        place: 'Küche',
-        love: 6,
-        user_id: 4
+        email: 'test@example.at',
+        password: 'slfjsflj2441!j',
+        nickName: 'suppiduppipflanzenboy'
     },
     {
-        realName: 'Kaktus',
-        nickName: 'Max',
-        waterNeed: 1200,
-        waterCurrent: 600,
-        sunNeed: 2.5,
-        place: 'Schlafzimmer',
-        love: 3,
-        user_id: 4
+        email: 'test1@example.at',
+        password: 'slfjsflj2441!j',
+        nickName: 'suppiduppipflanzenboyy'
+    },
+    {
+        email: 'benjamin.joham@gmail.com',
+        password: 'test1234',
+        nickName: 'Benni'
     }
 ])
+
+# Plant.create([
+#     {
+#         realName: 'Rose',
+#         nickName: 'Lisa',
+#         waterNeed: 1400,
+#         waterCurrent: 600,
+#         sunNeed: 2.5,
+#         place: 'Küche',
+#         love: 6,
+#         user_id: 4
+#     },
+#     {
+#         realName: 'Kaktus',
+#         nickName: 'Max',
+#         waterNeed: 1200,
+#         waterCurrent: 600,
+#         sunNeed: 2.5,
+#         place: 'Schlafzimmer',
+#         love: 3,
+#         user_id: 4
+#     }
+# ])
 
 # Plant.create([
 #     {
