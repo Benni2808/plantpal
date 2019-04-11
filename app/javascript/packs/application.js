792b6d7761ae61ae619dcc23dba6fd5ba44000cf
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-04-11 15:31:12 +0200

    imageresizing on upload

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,5 @@
+import { PlantIDUsage, PlantIDIdentification } from "../api/PlantId";
+import ImagTools from './ImageTools';
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -12,108 +14,73 @@
 
 // console.log('test');
 window.addEventListener('load', ()=>{
+  console.log(window.location.pathname);
+  const path = window.location.pathname;
+  
+  const ResizeImage = (image) => {
+    ImagTools.resize(image, {
+      width: 1200,
+      height: 1200
+    }, (blob,didItResize)=>{
+      let newForm = new FormData();
+      newForm.append('image', blob);
+      document.getElementById('preview').src = window.URL.createObjectURL(blob);
+
+      console.log(document.getElementById('plant_image').files[0]);
+      var newFile = blobToFile(blob, 'output.png');
+      console.log(newFile);
+      console.log(newFile.name);
+      let current_image = document.getElementById('plant_image');
+
+      const dt = new DataTransfer();
+      console.log(blob);
+      dt.items.add(new File([blob], newFile.name ));
+
+      // for (let file of current_image.files) {
+      //   if(file !== current_image.files[0]) {
+      //     dt.items.add(file);
+      //   }
+      // }
+      current_image.files = dt.files;
+      
+      console.log(document.getElementById('plant_image').files[0]);
+      function blobToFile(theBlob, fileName){
+        //A Blob() is almost a File() - it's just missing the two properties below which we will add
+        theBlob.lastModifiedDate = new Date();
+        theBlob.name = fileName;
+        return theBlob;
+    }
+    });
+  }
+  
+
     console.log('test');
 
-    const user_photo = document.getElementById('old_user_photo');
+    
 
-    user_photo.addEventListener('click', (e) => {
-        console.log(e);
-        startup();
-    });
+    // if (window.location.pathname)
+    
+    if(path.includes('plant')) {
+
+      document.getElementById('plant_image').onchange = (event) =>{
+        console.log(event);
+        ResizeImage(event.target.files[0])
+      };
+
+      const current_plant = document.getElementById('plant_photo');
+      current_plant.addEventListener('click', (e) => {
+        console.log(e.currentTarget);
+        var dataUrl = getDataUrl(event.currentTarget)
+        console.log(dataUrl)
+        PlantIDIdentification(dataUrl)
+      });
+    }
+
 
     /**
-     * WebRTC camera access request
+     * API request plant
      */
-    // define width, height is calculated later
-    let width = 320;   
-    let height = 0;   
-    let streaming = false;
-    // define some html elements
-    let video = null;
-    let canvas = null;
-    let photo = null;
-    let startbutton = null;
-
-    function startup() {
-      video = document.getElementById('video');
-      canvas = document.getElementById('canvas');
-      photo = document.getElementById('photo');
-      startbutton = document.getElementById('startbutton');
-      navigator.mediaDevices.getUserMedia({video: true, audio: false})
-      .then(function(stream) {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(function(err) {
-        console.log("An error occurred: " + err);
-      });
-      video.addEventListener('canplay', function(ev){
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth/width);
-          // fixes a bug in firefox
-          if (isNaN(height)) {
-            height = width / (4/3);
-          }
-          video.setAttribute('width', width);
-          video.setAttribute('height', height);
-          canvas.setAttribute('width', width);
-          canvas.setAttribute('height', height);
-          streaming = true;
-        }
-      }, false);
-      startbutton.addEventListener('click', function(ev){
-        takepicture();
-        ev.preventDefault();
-      }, false);
-      clearphoto();
-    }
-
-    function clearphoto() {
-      var context = canvas.getContext('2d');
-      context.fillStyle = "#AAA";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-      var data = canvas.toDataURL('image/png');
-      photo.setAttribute('src', data);
-    }
-
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
-
-    function takepicture() {
-      var context = canvas.getContext('2d');
-      if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(video, 0, 0, width, height);
-        var data = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");
-        // window.location.href=data;
-        var blob = canvas.toB
-        photo.setAttribute('src', data);
-        let file_field = document.getElementById('user_photo')
-        // console.lo
-        canvas.toBlob((blob) =>{
-            console.log(blob);
-            var url=window.URL.createObjectURL(blob);
-            let new_url = URL.revokeObjectURL(url);
-            console.log(new_url);
-            // file_field.files.append(data,blob.size, url);
-            console.log(`Super ${blob.size} amazing ${url}`);
-            file_field.files[0] = {name: 'test.jpg', size: 23424, type: 'image/png'};
-            image.src=url;
-            // file_field.value = url;
-            document.body.appendChild(image);
-            console.log(file_field.files); 
-        });
-      } 
-      else {
-        clearphoto();
-      }
-    }
-
-
-    // event listener to run startup once loading is done
-    // window.addEventListener('load', startup, false);
+    console.log(` Node Environment: ${process.env.NODE_ENV}`);
+    console.log(` Other Environment ${process.env.PLANT_ID_KEY}`);
+    console.log(PlantIDUsage());
 });
