e19da83c28295bc2d94b74bcb8fc8d6e449487c6
Johanna Repp <joshi.repp@gmail.com>
2019-04-12 11:11:52 +0200

    add love counter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,12 @@ class ApplicationController < ActionController::Base
         Rails.logger.warn('   we did it already, stopping.')
       else
         Plant.update_all('"waterCurrent" = "waterCurrent" - ("waterNeed" / 7.0)')
+        Plant.update_all('"love" = love-1')
         Rails.logger.warn('   finding all the all the water_current values that are less than 1/6 of the water_need values')
         Rails.logger.warn('   sending an e-mail reminder to the owners of the dry plants we just found')
       end
     end    
-    
+
     protected
 
     def configure_permitted_parameters
