921829a97e8141ff3393c24b5f28eb5b4a004ac3
Benjamin Joham <bjoham.mmt-b2017@fh-salzburg.ac.at>
2019-06-21 10:39:25 +0200

    add water route for watering plant

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :plants
+  resources :plants do
+    member do
+      patch 'water'
+    end
+  end
   # get 'sessions/destroy'
   root to: 'pages#home'
   get '/picture' => 'pages#picture'
